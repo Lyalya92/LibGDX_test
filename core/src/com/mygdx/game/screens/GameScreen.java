a2fb997464a1e7689efe17de8d54ba25cc9f5a50
Lyalya92 <lialia.suk@gmail.com>
2022-08-30 18:36:17 +0400

    Homework 5

--- core/src/com/mygdx/game/screens/GameScreen.java
+++ core/src/com/mygdx/game/screens/GameScreen.java
@@ -3,7 +3,6 @@ package com.mygdx.game.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -14,11 +13,10 @@ import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.mygdx.game.Direction;
-import com.mygdx.game.Main;
-import com.mygdx.game.MyAnimation;
-import com.mygdx.game.NinjaGirl;
+import com.mygdx.game.*;
 
 public class GameScreen implements Screen {
     private Main game;
@@ -31,15 +29,19 @@ public class GameScreen implements Screen {
     private OrthogonalTiledMapRenderer mapRenderer;
     private Vector2 camPosition;
     private Rectangle mapSize;
+
     private ShapeRenderer shapeRenderer;
+    private MyPhysic myPhysic;
+    private Body body;
+    private final Rectangle heroRect;
 
+    private final int [] bg;
+    private final int [] layers;
 
-    private final float FRAME_SHIFT = 3;
     private MyAnimation animation;
     private MyAnimation anmRun;
     private MyAnimation anmIdle;
 
-    private float x, y;
     private Direction runDirection;
 
     public GameScreen(Main game, NinjaGirl ninjaGirl) {
@@ -51,23 +53,31 @@ public class GameScreen implements Screen {
         batch = new SpriteBatch();
         shapeRenderer = new ShapeRenderer();
         img = new Texture("scene/BG.png");
-
+        myPhysic = new MyPhysic();
         camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         map = new TmxMapLoader().load("map/map1.tmx");
         mapRenderer = new OrthogonalTiledMapRenderer(map);
         camPosition = new Vector2();
+        RectangleMapObject tmp = (RectangleMapObject)map.getLayers().get("setting").getObjects().get("hero");
+        body = myPhysic.addObject(tmp);
+        heroRect = tmp.getRectangle();
 
-        ((RectangleMapObject)map.getLayers().get("objects").getObjects().get("camera"))
-                .getRectangle().getPosition(camPosition);
-
-        camera.position.x = camPosition.x;
-        camera.position.y = camPosition.y;
 
         mapSize = ((RectangleMapObject)map.getLayers().get("objects").getObjects().get("border"))
                 .getRectangle();
 
+        bg = new int[1];
+        layers = new int[2];
+        bg[0] = map.getLayers().getIndex("background");
+        layers[0] = map.getLayers().getIndex("decoration_l1");
+        layers[1] = map.getLayers().getIndex("decoration_l2");
+
+
+        Array<RectangleMapObject> objects = map.getLayers().get("objects").getObjects().getByType(RectangleMapObject.class);
+        for (RectangleMapObject obj: objects) {
+            myPhysic.addObject(obj);
+        }
         runDirection = Direction.STAND;
-        y = 190;
     }
 
     @Override
@@ -77,6 +87,8 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
+        camera.position.x = body.getPosition().x;
+        camera.position.y = body.getPosition().y;
         camera.update();
 
         ScreenUtils.clear(0, 0, 0, 1);
@@ -86,16 +98,18 @@ public class GameScreen implements Screen {
         if (Gdx.input.isKeyPressed(Input.Keys.UP) && camera.zoom >0) camera.zoom -= 0.01;
 
         if ((Gdx.input.isKeyPressed(Input.Keys.A) || Gdx.input.isKeyPressed(Input.Keys.LEFT))
-               && (mapSize.x < camera.position.x-1)) {
+                && (mapSize.x < body.getPosition().x-1))
+        {
+            body.applyForceToCenter(new Vector2(-1000000,0), true);
             runDirection = Direction.LEFT;
             animation = anmRun;
-            camera.position.x -= FRAME_SHIFT;
         }
+
         if ((Gdx.input.isKeyPressed(Input.Keys.D) || Gdx.input.isKeyPressed(Input.Keys.RIGHT))
-                && (camera.position.x < mapSize.x + mapSize.width-1)) {
+                && (body.getPosition().x < mapSize.x + mapSize.width-1)) {
+            body.applyForceToCenter(new Vector2(1000000,0), true);
             runDirection = Direction.RIGHT;
             animation = anmRun;
-            camera.position.x += FRAME_SHIFT;
         }
         if (!Gdx.input.isKeyPressed(Input.Keys.ANY_KEY)) {
             runDirection = Direction.STAND;
@@ -105,26 +119,37 @@ public class GameScreen implements Screen {
         if ((!animation.isRotated() && runDirection == Direction.LEFT)
                 || (animation.isRotated() && runDirection == Direction.RIGHT)) animation.rotate();
 
+        mapRenderer.setView(camera);
+        mapRenderer.render(bg);
+
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
         batch.draw(img, 0, 0);
-        batch.draw(animation.getFrame(), camera.position.x, y);
-        x += FRAME_SHIFT * runDirection.getValue();
+        heroRect.x = body.getPosition().x;
+        heroRect.y = body.getPosition().y;
+        batch.draw(animation.getFrame(), heroRect.x - heroRect.width/2, heroRect.y - heroRect.height/2, heroRect.width, heroRect.height);
         batch.end();
         mapRenderer.setView(camera);
-        mapRenderer.render();
+        mapRenderer.render(bg);
 
+        mapRenderer.render(layers);
+
+        myPhysic.step();
+        myPhysic.debugDraw(camera);
 
         if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
             dispose();
             game.setScreen(new MenuScreen(this.game, this.ninjaGirl));
         }
 
-        shapeRenderer.setProjectionMatrix(camera.combined);
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
-        shapeRenderer.setColor(Color.RED);
-        shapeRenderer.rect(mapSize.x, mapSize.y, mapSize.width, mapSize.height);
-        shapeRenderer.end();
+//        shapeRenderer.setProjectionMatrix(camera.combined);
+//        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+//        shapeRenderer.setColor(Color.RED);
+//        for (RectangleMapObject obj: objects) {
+//            Rectangle rect = obj.getRectangle();
+//            shapeRenderer.rect(rect.x, rect.y, rect.width, rect.height);
+//        }
+//        shapeRenderer.end();
     }
 
     @Override
